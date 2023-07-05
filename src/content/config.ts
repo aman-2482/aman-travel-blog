87562d46c0bdf0ab6512700299eff174377e0655
aman-2482 <amanc273014@gmail.com>
2023-07-05 14:01:27 +0530

    c

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,8 @@
 import { defineCollection } from "astro:content";
 import { blogSchema } from "./_schemas";
-import { blogerSchema } from "./_schemas";
 
 const blog = defineCollection({
   schema: blogSchema,
 });
 
-
-const bloger = defineCollection({
-  schema: blogerSchema,
-});
-
-export const collections = { bloger };
 export const collections = { blog };
